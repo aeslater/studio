e7a0a4fc925aae398c02ce980b6e26de1e690dbc
Ashley <aeslater@gmail.com>
2020-07-23 13:59:32 -0500

    making progress on countdown timer and week number

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ console.log('This file is connected and ready to run.');
 //          This section is for a countdown timer showing days until music school starts (during summer)
 //          Or for a week counter showing which week of the school year we are in.
 
-var today // Determine today's date.
-var summerStart // first Saturday of May.
-var schoolStart // first Sunday of September after Labor day.
+var today = new Date();// Determine today's date.
+var summerStart = '2020-05-02' // first Saturday of May.
+var schoolStart = '2020-09-06' // first Sunday of September after Labor day.
 var daysLeft // this is how many days of summer left, or number of days until music school starts
 
-var sunday // during the school year, the week starts on Sunday 
-var saturday // during the school year, the week ends on Saturday
 var thisWeek // this is the week number of the school year, from 1 to 40. I'm continuing right through Christmas or else I'll never get done with this assignment.
 
 // If it's summer, display a countdown days timer until school starts.
-// 
-//  
-// if (today >= summerStart && today < schoolStart) {
-//   daysLeft = schoolStart - today
-//}
-// If it's during school year, display week number thisWeek
+if (today >= summerStart && today < schoolStart) {
+    daysLeft = schoolStart - today;
+    document.write(<p>'Days until Music School Starts:' + daysLeft</p>);
+}
 
-// if (today >= schoolStart && today < schoolStart+6) {
-//  week = 1;
-//  l
-// }
-// if (today < summerStart || today >=schoolStart) {display thisWeek}
+// If it's during school year but before the next calendar year (i.e. fall semester), display the week # of the school year
+else if (today >= schoolStart) {
+    thisWeek = Math.floor((today - schoolStart) / 7) // Round down.
+    document.write(<p>'We are in Week ' + thisWeek + ' of 36'</p>)
+}
+
+// If it's during school year and before summer (i.e. spring semester), display the week # of the school year
+// Adding number of weeks in spring semester plus 16 weeks for last calendar year's fall semester
+else {
+    thisWeek = Math.floor((today / 7) + 16)  
+    document.write(<p>'We are in Week ' + thisWeek + ' of 36'</p>)
+}
+
 
 
 
@@ -39,12 +43,12 @@ var instrument = prompt('Do you play an instrument? (yes or no)');
 // This will be the reply to the user
 var reply;
 
-if(instrument = 'no'|| 'No' || 'NO' || 'N' || 'n') {
+if (instrument = 'no'|| 'No' || 'NO' || 'N' || 'n') {
     reply = 'Check out our instruments!';
     window.alert(reply);
     // figure out how to highlight instruments section
 
-} else if(instrument = 'yes' || 'Yes' || 'YES' || 'y' || 'Y') {
+} else if (instrument = 'yes' || 'Yes' || 'YES' || 'y' || 'Y') {
 
     // If user plays an instrument, did they practice today?
     var practice = prompt('Did you practice today? (yes or no)');
