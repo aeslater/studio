68ddb3f6f4cd8f2260f6fc200c920064d8b865db
Ashley <aeslater@gmail.com>
2020-07-25 10:16:16 -0500

    improved counter and user prompts and alerts

--- script.js
+++ script.js
@@ -14,23 +14,24 @@ var daysLeft; // this is how many days of summer left, or number of days until m
 
 var thisWeek; // this is the week number of the school year, from 1 to 36. I'm continuing right through Christmas or else I'll never get done with this assignment.
 
+
 // If it's summer, display a countdown days timer until school starts.
 if (today >= summerStart && today < schoolStart) {
-    daysLeft = schoolStart - today;
-    document.write(<p>'Days until Music School Starts:' + daysLeft</p>);
-}
+    daysLeft = Math.round((schoolStart - today)/(1000*60*60*24)+1);
+    document.write(daysLeft + ' Days until <br> Music School Starts!');
+    }
 
 // If it's during school year but before the next calendar year (i.e. fall semester), display the week # of the school year
 else if (today >= schoolStart) {
     thisWeek = Math.floor((today - schoolStart) / 7); // Round down.
-    document.write(<p>'We are in Week ' + thisWeek + ' of 36'</p>);
+    document.write('We are in<br>Week ' + thisWeek + ' of 36');
 }
 
 // If it's during school year and before summer (i.e. spring semester), display the week # of the school year
 // Adding number of weeks in spring semester plus 16 weeks for last calendar year's fall semester
 else {
     thisWeek = Math.floor((today / 7) + 16);
-    document.write(<p>'We are in Week ' + thisWeek + ' of 36'</p>);
+    document.write('We are in<br>Week ' + thisWeek + ' of 36');
 }
 
 
@@ -42,11 +43,9 @@ else {
 // Ask user if they play an instrument 
 var instrument = prompt('Do you play an instrument? (yes or no)').toLowerCase().trim();
 
-// This will be the reply to the user
-var reply;
-
 if (instrument === 'no' || instrument === 'n') {
     alert('Check out our instruments!');
+    
     // right now in style.css line 84 sets .instruments background-color to cadet blue
     // need to set .instruments background-color to darkgoldenrod
     // figure out how to highlight instruments section
