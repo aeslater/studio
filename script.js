4ada95da31f8ca1af476db12f83a11119cc04fc5
Ashley <aeslater@gmail.com>
2020-07-25 15:33:58 -0500

    got data validation and functions to work for user input questions

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ var thisWeek; // this is the week number of the school year, from 1 to 36. I'm c
 // If it's summer, display a countdown days timer until school starts.
 if (today >= summerStart && today < schoolStart) {
     daysLeft = Math.round((schoolStart - today)/(1000*60*60*24)+1);
-    document.write(daysLeft + ' Days until <br> Music School Starts!');
+    document.write(daysLeft + ' Days until <br> Music School Starts!'); 
+    // Please use .textContent or .innerHTML and avoid document.write()
     }
 
 // If it's during school year but before the next calendar year (i.e. fall semester), display the week # of the school year
@@ -40,8 +41,41 @@ else {
 //           This section is for a question about whether the user plays an instrument (check out instruments if you don't play) 
 //           And a question if they practiced (with GIFs celebrating and encouraging practice). 
 
-// Ask user if they play an instrument 
-var instrument = prompt('Do you play an instrument? (yes or no)').toLowerCase().trim();
+'use strict';
+console.log('Hello.');
+
+
+// Wrap related steps into a single function. Be sure you are properly defining the function, calling it, and getting the return value you expect.
+// Add some kind of validation to the user input you are getting, to ensure only acceptable values are given.
+
+// Asks user if they play instrument, returns the answer, logs answer
+function inputInstrument() { 
+  var question = 'Do you play an instrument? (yes or no)';
+  var instrument = prompt(question).toLowerCase().trim();
+  return validateInput(question, instrument);
+}
+
+// Asks user if they practiced, returns the answer, logs answer
+function inputPractice() {
+  var question = 'Did you practice today? (yes or no)';
+  var practice = prompt(question).toLowerCase().trim();
+  return validateInput(question, practice);
+}
+
+// Checks if answer is valid format, if invalid returns false and logs false
+function validateInput(question, response) {
+    while (response !== 'yes' && response !== 'y' && response !== 'no' && response !== 'n') {
+    alert('Please enter yes or no.');
+    response = prompt(question).toLowerCase().trim();
+    } 
+    return response;
+}
+
+// Calls the function to input if user plays an instrument
+var instrument = inputInstrument();
+
+// Calls function to validate input, while data is invalid displays an alert then asks for input again, returns re-entered input
+
 
 if (instrument === 'no' || instrument === 'n') {
     alert('Check out our instruments!');
@@ -53,24 +87,16 @@ if (instrument === 'no' || instrument === 'n') {
 } else if (instrument === 'yes' || instrument === 'y') {
 
     // If user plays an instrument, did they practice today?
-    var practice = prompt('Did you practice today? (yes or no)').toLowerCase().trim();
+    // Calls the function to input if user practiced today
+    var practice = inputPractice();
 
     if (practice === 'yes' || practice === 'y') {
         alert('Great job!'); // + <img src = "images/celebrate1.gif/"></img>
         // Figure out how to display a celebratory GIF. I tried adding inside the alert parameters but no success.
 
     } else if (practice === 'no' || practice === 'n') {
-        alert('Practice only on the days you eat. -Dr. Shinichi Suzuki'); // + <img src = "images/youcandoit.gif/"></img>
+        alert('Practice only on the days you eat. -Dr. Shinichi Suzuki. You can do it!'); 
+        // + <img src = "images/youcandoit.gif/"></img>
         // Figure out how to display a "You can do it!" GIF. 
-
-    } else {
-        alert("I don't understand. Please refresh and try again.");
     }
-
-} else {
-    alert("I don't understand. Please refresh and try again.");
 } 
-
-
-    
-
